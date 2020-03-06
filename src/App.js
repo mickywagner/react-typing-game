8417836b50308a3c0fbd53e1cfca89a5191a78c3
Michaela Wagner <mickywagner@gmail.com>
2020-03-05 17:28:16 -0700

    Change font size, add useState for textarea

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [ text, setText ] = useState('Start typing...')
+
+  function updateText(e) {
+    const { value } = e.target
+    setText(value)
+  }
+
+  console.log(text)
+
   return (
     <div className="App">
       <div>
             <h1>Test Your Typing Speed</h1>
             <form>
-                <textarea placeholder="Start typing" />
+                <textarea 
+                  value={text}
+                  onChange={updateText}
+                />
             </form>
             <h4>Time remaing: 4 seconds</h4>
             <button>Start Game</button>
