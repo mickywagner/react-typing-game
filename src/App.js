156b56ebc2f20d97e4a78b59f103f1608283ef12
Michaela Wagner <mickywagner@gmail.com>
2020-03-05 17:14:38 -0700

    Basic structure of app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ function App() {
   return (
     <div className="App">
       <div>
-        Typing Game
-        
-      </div>
+            <h1>Test Your Typing Speed</h1>
+            <form>
+                <textarea placeholder="Start typing" />
+            </form>
+            <h4>Time remaing: 4 seconds</h4>
+            <button>Start Game</button>
+            <h1>Word Count: 0</h1>
+        </div>
     </div>
   );
 }
