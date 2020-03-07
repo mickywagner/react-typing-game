06246f0b69c1600a2798a2e80ac2c517aa8c68c8
Michaela Wagner <mickywagner@gmail.com>
2020-03-07 03:27:47 -0700

    Updates

---
+++ precache-manifest.9319cae4b53237840c2476d64e70b6ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a7bc5bd83d7f5cab904515b1fa85c1",
+    "url": "/react-typing-game/index.html"
+  },
+  {
+    "revision": "076c26a5f30e70d8aeb8",
+    "url": "/react-typing-game/static/css/main.0ab77b13.chunk.css"
+  },
+  {
+    "revision": "2f3fc97ff9acb4bb720d",
+    "url": "/react-typing-game/static/js/2.2b59c952.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-typing-game/static/js/2.2b59c952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "076c26a5f30e70d8aeb8",
+    "url": "/react-typing-game/static/js/main.4e3c9078.chunk.js"
+  },
+  {
+    "revision": "6f4d7459077b44e9964a",
+    "url": "/react-typing-game/static/js/runtime-main.21510c9b.js"
+  }
+]);
